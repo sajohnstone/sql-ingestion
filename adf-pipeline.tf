de3c66cddb5d04ee983b0aaf19c2c593fb682fc5
Stu Johnstone <sajohnstone@yahoo.co.uk>
2024-10-20 18:15:13 +1100

    add adf

---
+++ adf-pipeline.tf
@@ -0,0 +1,32 @@
+resource "azurerm_data_factory_pipeline" "taxi_incremental" {
+  name            = "${local.name}-taxi-incremental"
+  data_factory_id = azurerm_data_factory.this.id
+
+  activities_json = jsonencode([
+    {
+      "name" : "CopyDataActivity",
+      "type" : "Copy",
+      "inputs" : [
+        {
+          "referenceName" : azurerm_data_factory_dataset_sql_server_table.source_dataset.name,
+          "type" : "DatasetReference"
+        }
+      ],
+      "outputs" : [
+        {
+          "referenceName" : azurerm_data_factory_dataset_azure_blob.blob_dataset.name,
+          "type" : "DatasetReference"
+        }
+      ],
+      "source" : {
+        "type" : "SqlSource"
+      },
+      "sink" : {
+        "type" : "BlobSink",
+        "format" : {
+          "type" : "ParquetFormat"
+        }
+      }
+    }
+  ])
+}
--- adf.tf
+++ adf.tf
@@ -7,3 +7,32 @@ resource "azurerm_data_factory" "this" {
     type = "SystemAssigned"
   }
 }
+
+resource "azurerm_data_factory_linked_service_sql_server" "source_db" {
+  name              = "${local.name}-sql-link"
+  data_factory_id   = azurerm_data_factory.this.id
+  connection_string = "Server=tcp:${azurerm_mssql_server.this.name}.database.windows.net,1433;Initial Catalog=${azurerm_mssql_database.this.name};User ID=${azurerm_mssql_server.this.administrator_login};Password=${var.sql_server_password};Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  depends_on        = [azurerm_mssql_server.this]
+}
+
+resource "azurerm_data_factory_dataset_sql_server_table" "source_dataset" {
+  name                = "${local.name}-sql-ds"
+  data_factory_id   = azurerm_data_factory.this.id
+  linked_service_name = azurerm_data_factory_linked_service_sql_server.source_db.name
+  table_name = "dbo_TaxiData_CT"
+}
+
+resource "azurerm_data_factory_linked_service_azure_blob_storage" "storage" {
+  name              = "${local.name}-storageacc-link"
+  data_factory_id   = azurerm_data_factory.this.id
+  connection_string = "DefaultEndpointsProtocol=https;AccountName=${azurerm_storage_account.this.name};AccountKey=<your_storage_account_key>;EndpointSuffix=core.windows.net"
+}
+
+resource "azurerm_data_factory_dataset_azure_blob" "blob_dataset" {
+  name                = "${local.name}-ds"
+  data_factory_id   = azurerm_data_factory.this.id
+  linked_service_name = azurerm_data_factory_linked_service_azure_blob_storage.storage.name
+
+  path     = "taxi_data_cdc"
+  filename = "data.parquet"
+}
